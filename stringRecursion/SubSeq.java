a416a1f9c0f44b3fcf2577376debae39a63a3e57
SayaliG123 <sayalighadge0902@gmail.com>
2026-05-18 19:56:15 +0530

    strings question by recusrion

--- stringRecursion/SubSeq.java
+++ stringRecursion/SubSeq.java
@@ -1,10 +1,13 @@
 package stringRecursion;
-//for finding all subsequence of array
+import java.util.ArrayList;
+import java.util.Arrays;
+//for finding all subsequence of array(32)
 
 public class SubSeq {
 
 	public static void main(String[] args) {
 		subseq(" ","abc");
+		System.out.println((subset(" ","abc")));
 
 	}
 	public static void subseq(String p,String up)
@@ -21,6 +24,23 @@ public class SubSeq {
 		subseq(p,up.substring(1)); //here we are ignoring the character
 		
 			
+	}
+	
+	public static ArrayList<String> subset(String p,String up)
+	{
+		if(up.isEmpty())
+		{
+			ArrayList<String>list=new ArrayList<>();
+			list.add(p);
+			return list;
+		}
+		char ch=up.charAt(0);
+		ArrayList<String>left=subset(p+ch,up.substring(1));//left subtree
+		ArrayList<String>right=subset(p,up.substring(1));//right subtree
+		left.addAll(right);//combining both
+		return left;
+		
+		
 	}
 
 }
