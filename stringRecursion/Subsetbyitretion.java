3d668fa0bca9275976cd718f97f214fe43627c2f
SayaliG123 <sayalighadge0902@gmail.com>
2026-05-21 16:34:19 +0530

    solved same question fro duplicates

--- stringRecursion/Subsetbyitretion.java
+++ stringRecursion/Subsetbyitretion.java
@@ -1,16 +1,23 @@
 package stringRecursion;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-//for finding all subset by iteration
+//here we are making subsets by iteration instate of recursion
 
 public class Subsetbyitretion {
 
 	public static void main(String[] args) {
-		int [] arr= {1,2,3};
-		List<List<Integer>> ans=subset(arr);
+		//int [] arr= {1,2,3};
+		int []arr1= {1,2,2};
+		/*List<List<Integer>> ans=subset(arr);
 		for(List<Integer> list:ans)
 		{
 			System.out.println(list);
+		}*/
+		List<List<Integer>> ans1=subsetduplicate(arr1);
+		for(List<Integer> list1:ans1)
+		{
+			System.out.println(list1);
 		}
 		
 
@@ -34,6 +41,35 @@ public class Subsetbyitretion {
 			}
 		}
 		return outer;
+	}
+	static List<List<Integer>> subsetduplicate(int [] arr)
+	{
+		Arrays.sort(arr);
+		List<List<Integer>> outer=new ArrayList<>();
+		outer.add(new ArrayList<>());
+		
+		int start=0;
+		int end=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			//if previous of arr is equal to current then s=e+1
+			if(i<0 && arr[i]==arr[i-1])
+			{
+				start=end+1;
+				
+			}
+			end=outer.size();
+			int n=outer.size();
+			for(int j=start;j<n;j++)
+			{
+				List<Integer>inner=new ArrayList<>(outer.get(j));
+				inner.add(arr[i]);
+				outer.add(inner);
+			}
+		}
+		return outer;
+		
+		
 	}
 
 }
