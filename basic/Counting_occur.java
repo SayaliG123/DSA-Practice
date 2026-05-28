fbcb25d6ac9a01c709c6e4b714ec4f310e53e7b0
SayaliG123 <sayalighadge0902@gmail.com>
2026-05-28 21:55:21 +0530

    counting occurence

---
+++ basic/Counting_occur.java
@@ -0,0 +1,30 @@
+package basic;
+import java.util.Scanner;
+//here we are finding that how many times that particular
+//number has arrived in given string
+
+public class Counting_occur {
+
+	public static void main(String[] args) {
+		System.out.println("enter the string ");
+		Scanner input=new Scanner(System.in);
+		int n = input.nextInt();
+		System.out.println("enter a number to seach");
+		int a=input.nextInt();
+		int count =0;
+		
+		while(n>0)
+		{
+			int rem=n%10;
+			if(rem==a)
+			{
+				count++;
+			}
+			n=n/10;
+		}
+		System.out.println(count);
+		
+
+	}
+
+}
